a744b478cf73bceced743f4baeaa85496cd7457f
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-12 19:17:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index(0) do |name, index|
-    line.push("#{index}.#{name}")
+    katz_deli.each.with_index(1) do |name, index|
+    line.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line.join(" ")}" #display who's in the line currently
   end
