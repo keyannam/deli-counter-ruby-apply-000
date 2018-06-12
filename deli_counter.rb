3d24240c53ddc4adc6769156a31a9704b3ee612c
keyannamayfield@gmail.com <keyannamayfield@gmail.com>
2018-06-12 19:30:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,10 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+def take_a_number
+  
+end 
+
 def now_serving(next_person)
   if next_person.empty? #is the line empty?
     puts "There is nobody waiting to be served!"
